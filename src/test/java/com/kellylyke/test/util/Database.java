912469c41f3fb9d470f20daca108d65dbf55ee17
Kelly Lyke <klyke@madisoncollege.edu>
2019-03-11 19:43:20 -0500

    Took 3 minutes

--- src/test/java/com/kellylyke/test/util/Database.java
+++ src/test/java/com/kellylyke/test/util/Database.java
@@ -77,7 +77,7 @@ public class Database {
             try {
                 connection.close();
             } catch (SQLException e) {
-                System.out.println("Cannot close connection" + e);
+                logger.error("Cannot close connection" + e);
             }
         }
 
